74852902a1cd01fd5258d7843a954dba35ec7086
Misterpetchz <petch.t1507@gmail.com>
2024-10-25 22:24:16 +0700

    feat(swagger): update api property

--- src/modules/branches/dto/create-branch.dto.ts
+++ src/modules/branches/dto/create-branch.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import {
   IsNotEmpty,
   IsOptional,
@@ -6,14 +7,26 @@ import {
 } from 'class-validator'
 
 export class CreateBranchDto {
+  @ApiProperty({
+    description: 'The name of the branch',
+    example: 'Central Plaza Branch',
+  })
   @IsString()
   @IsNotEmpty()
   branchName: string
 
+  @ApiProperty({
+    description: 'The address of the branch',
+    example: '1234 Main St, Bangkok, Thailand',
+  })
   @IsString()
   @IsNotEmpty()
   address: string
 
+  @ApiPropertyOptional({
+    description: 'The phone number of the branch',
+    example: '+66812345678',
+  })
   @IsPhoneNumber('TH')
   @IsOptional()
   phoneNumber?: string
--- src/modules/profiles/dto/create-profile.dto.ts
+++ src/modules/profiles/dto/create-profile.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import {
   IsArray,
   IsDateString,
@@ -9,26 +10,50 @@ import {
 } from 'class-validator'
 
 export class CreateCustomerProfileDto {
+  @ApiProperty({
+    description: 'Your first name',
+    example: 'john',
+  })
   @IsString()
   readonly firstName: string
 
+  @ApiProperty({
+    description: 'Your last name',
+    example: 'Doe',
+  })
   @IsString()
   readonly lastName: string
 
+  @ApiPropertyOptional({
+    description: "The customer's phone number",
+    example: '+66812345678',
+  })
   @IsPhoneNumber('TH')
   @IsOptional()
   readonly phoneNumber?: string
 
+  @ApiPropertyOptional({
+    description: "The customer's date of birth in ISO format",
+    example: '1990-01-01',
+  })
   @IsDateString()
   @IsOptional()
   readonly birthDate?: string
 }
 
 export class CreateRestaurantProfileDto {
+  @ApiProperty({
+    description: 'The name of the restaurant',
+    example: 'Hiso fries chicken',
+  })
   @IsString()
   @IsNotEmpty()
   readonly restaurantName: string
 
+  @ApiPropertyOptional({
+    description: 'The address of the restaurant',
+    example: '1234 Thai Street, Bangkok, Thailand',
+  })
   @IsString()
   @IsOptional()
   readonly address: string
